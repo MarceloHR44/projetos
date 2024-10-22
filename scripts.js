922283102d833b30bca3b601b1d08a30eb969f05
bootcamp753 <93229581+bootcamp753@users.noreply.github.com>
2024-10-22 13:07:31 -0300

    Meu Primeiro Projeto

---
+++ scripts.js
@@ -0,0 +1,41 @@
+
+
+let botaoSom = document.querySelector(".botao-som")
+let video = document.querySelector(".video")
+let botao = document.querySelector(".link-info")
+let modal = document.querySelector(".modal")
+let audio = document.querySelector(".audio")
+
+
+// Ligar o som
+botaoSom.addEventListener("click", ligaSom)
+
+function ligaSom() {
+    if (video.muted) {
+        video.muted = false
+    } else {
+        video.muted = true
+    }
+}
+
+
+// Mostrar Modal
+botao.addEventListener("click", mostrarModal)
+modal.addEventListener("click", esconderModal)
+
+function mostrarModal() {
+    modal.style.display = "block"
+}
+
+function esconderModal() {
+    modal.style.display = "none"
+}
+
+// tocar o tundum
+window.addEventListener("load", tocarAudio)
+
+function tocarAudio() {
+    audio.play()
+}
+
+
